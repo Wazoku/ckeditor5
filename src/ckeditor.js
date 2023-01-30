655e36519b7ff7bd8999e6fe167fd7246ec3613e
JOHN1202 <john_gaballah@yahoo.com>
2023-01-30 15:21:30 +0000

    adding font plugin for all font features

--- src/ckeditor.js
+++ src/ckeditor.js
@@ -34,6 +34,7 @@ import TextTransformation from '@ckeditor/ckeditor5-typing/src/texttransformatio
 import CloudServices from '@ckeditor/ckeditor5-cloud-services/src/cloudservices';
 import Underline from '@ckeditor/ckeditor5-basic-styles/src/underline';
 import Strikethrough from '@ckeditor/ckeditor5-basic-styles/src/strikethrough';
+import Font from '@ckeditor/ckeditor5-font/src/font';
 
 export default class ClassicEditor extends ClassicEditorBase {}
 
@@ -66,7 +67,8 @@ ClassicEditor.builtinPlugins = [
 	TableToolbar,
 	TextTransformation,
 	Underline,
-	Strikethrough
+	Strikethrough,
+	Font
 ];
 
 // Editor configuration.
@@ -84,6 +86,9 @@ ClassicEditor.defaultConfig = {
 			'underline',
 			'strikethrough',
 			'fontSize',
+			'fontFamily',
+			'fontColor',
+			'fontBackgroundColor',
 			'link',
 			'bulletedList',
 			'numberedList',
